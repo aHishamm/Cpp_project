e633acbf5bc212306fef0d0fdbc8e4f39f59a979
Abdulrahman Al Muaitah <a.almuaitah@gmail.com>
2024-02-08 12:31:51 +0400

    Version 1.0 of the project

---
+++ app.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std; 
+int multiply(int x, int y); 
+int main(){
+
+
+    cout << multiply(3,4) << "\n"; 
+
+    return 0;
+}
+int multiply(int x, int y){
+
+    return x * y; 
+}
