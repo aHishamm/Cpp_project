4a59fa90fb55ea2597e5ef917d9f9d00089ee2d8
Abdulrahman Al Muaitah <a.almuaitah@gmail.com>
2024-02-08 12:35:41 +0400

    Version 2.0 added factorial function

--- app.cpp
+++ app.cpp
@@ -1,14 +1,24 @@
 #include <iostream> 
 using namespace std; 
 int multiply(int x, int y); 
+int factorial(int x); 
 int main(){
 
 
     cout << multiply(3,4) << "\n"; 
+    cout << factorial(2) << "\n"; 
 
     return 0;
 }
 int multiply(int x, int y){
 
     return x * y; 
+}
+int factorial(int x){ 
+    if(x < 2){ 
+        return 1; 
+    }
+    else{
+        return x * factorial(x-1);
+    }
 }
